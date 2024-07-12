0b3955e103dedcf6a2ca41bacafd2bcc5b24d0cd
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-07-12 10:25:32 -0700

    debugging issue, .env was in the wrong location cause error when reaching env file. also, add styling

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,10 @@ import { ACCESS_TOKEN } from "./constants"
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
+  // baseURL: "http://127.0.0.1:8000"
 })
+
+console.log(import.meta.env.VITE_API_URL);
 
 api.interceptors.request.use(
   (config) =>{
